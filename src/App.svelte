1de61b306b2dc8af009cd8171fcfaed0029f6cb3
TJ-Albertson <THOMASJOHNNY77@GMAIL.COM>
2022-09-22 14:58:57 -0500

    svg, site boxes formatting, site component
    adding svg for site header,  small css and html changes, added site component but not using

--- src/App.svelte
+++ src/App.svelte
@@ -1,44 +1,84 @@
 <script>
-  export let name;
-
-  let mountains = '../public/mountains.jpg'
+  import Cloudbox from "./SVG/cloudbox.svelte";
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
   <h1>TJ Albertson</h1>
-  <h3>FullStack Developer</h3>
+  <h2>Fullstack Developer</h2>
   <hr />
   <div class="sites-flex">
-    <div class="site" >
-		<h2>Cloudbox</h2>
-		<img {mountains}/>
-  	</div>
-    <div class="site" >
-		<h2>The Colosseum</h2>
-    	<img />
-  	</div>
-	</div>
-	  <hr />
-  <p>
-    Contact Info + Links
-  </p>
+    <div class="site">
+      <div class="site-header">
+        <Cloudbox/>
+        <h2> Cloudbox</h2>
+      </div>
+      
+      <img src="images/mountains.jpg" />
+      <p>Repositories</p>
+      <hr />
+      <div class="links">
+        <a href="">Cloudbox</a> | <a href="">API</a> |
+        <a href="">Logs</a>
+      </div>
+    </div>
+    <div class="site">
+      <div class="site-header">
+        <Cloudbox/>
+        <h2>The Colosseum</h2>
+      </div>
+
+     
+      <img src="images/nature.jpg" />
+      <p>Repositories</p>
+      <hr />
+      <div class="links">
+        <a href="">The Colosseum</a> | <a href="">Logs</a>
+      </div>
+    </div>
+  </div>
+  <hr />
+  <p>Contact Info + Links</p>
 </main>
 
 <style>
-	p {
-		display: flex;
-		flex-direction: column	;
-	}
-  .site {
-	  border-style: solid;
+  .links {
+    margin-bottom: 15px;
+  }
+
+  img {
+    height: 350px;
+    width: 700px;
     margin: 15px;
+  }
+
+  p {
+    display: flex;
+    flex-direction: column;
+  }
+  .site {
+    border-style: solid;
+    margin: 25px;
+    margin-left: 55px;
+    margin-right: 55px;
+    width: 50rem;
+  }
+
+  svg {}
+
+  .site-header {
+    display: inline-flex;
+    font-family: sans-serif;
+    font-size: 40px;
+    margin: 15px;
+    gap: 15px;
+    align-items: center;
+    height: 80px;
   }
 
   .sites-flex {
     display: flex;
     align-items: center;
-  justify-content: center;
+    justify-content: center;
   }
 
   main {
---
+++ src/Site.svelte
@@ -0,0 +1,49 @@
+<script>
+    import Icon from './Icon.svelte';
+  </script>
+  
+  <main>
+      <div class="site">
+        <h2><Icon name="cloudbox" />Cloudbox</h2>
+        <img src="images/mountains.jpg" />
+        <p>Repositories</p>
+        <hr />
+        <div class="links">
+          <a href="">Cloudbox</a> | <a href="">API</a> |
+          <a href="">Logs</a>
+        </div>
+      </div>
+  </main>
+  
+  <style>
+    .links {
+      margin-bottom: 15px;
+    }
+  
+    .site {
+      border-style: solid;
+      margin: 25px;
+      margin-left: 55px; 
+      margin-right: 55px; 
+    }
+  
+    main {
+      text-align: center;
+      padding: 1em;
+      max-width: 240px;
+      margin: 0 auto;
+    }
+  
+    h1 {
+      color: #ff3e00;
+      text-transform: uppercase;
+      font-size: 4em;
+      font-weight: 100;
+    }
+  
+    @media (min-width: 640px) {
+      main {
+        max-width: none;
+      }
+    }
+  </style>
